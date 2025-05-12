22966dbaf5188a8391f6d002097a029b314569a3
0Slmpnv0 <142727420+0Slmpnv0@users.noreply.github.com>
2025-05-13 02:02:44 +0600

    Merge the actual ssl setup changes (#2)
    The nginx image is changed for better ssl integration

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -16,11 +16,7 @@ export interface VacancyFull {
   connection: string;
 }
 
-<<<<<<< Updated upstream
-const API_URL = 'http://api.localhost';
-=======
 const API_URL = 'https://api.startlab.site';
->>>>>>> Stashed changes
 
 export async function getVacanciesList(): Promise<VacancyShort[]> {
   const res = await fetch(`${API_URL}/vacancies/list`);
